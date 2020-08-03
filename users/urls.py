811d0b7dbb0b5efa751e229ce0e4a506ed1c790e
“princekrroshan01” <princekrroshan01@gmail.com>
2020-08-03 11:05:40 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('upload', views.upload, name='upload'),
+]
